6e8da2679c323134c65a3ed595f60aef5cdc6417
Steve <steveghking@gmail.com>
2024-10-08 11:17:32 +0200

    Update resumejs.js

--- resumejs.js
+++ resumejs.js
@@ -13,60 +13,88 @@ document.addEventListener("DOMContentLoaded", function() {
       <div class="col-lg-6">
         <h3 class="resume-title">Summary</h3>
         <div class="resume-item pb-0">
-          <h4>Alice Barkley</h4>
-          <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+          <h4>Stephen Adjei</h4>
+          <p><em>Innovative and deadline-driven Data sceintist with 7+ years of experience designing and developing user-centered problem soling models from initial concept to final, improve and scale exiting automotion systems.</em></p>
           <ul>
-            <li>Portland par 127, Orlando, FL</li>
-            <li>(123) 456-7891</li>
-            <li>alice.barkley@example.com</li>
+            <li>Koblenz, Germany, DE</li>
+            <li>(+49) 1234-56789</li>
+            <li>steveking@example.com</li>
           </ul>
         </div>
 
         <h3 class="resume-title">Skills</h3>
         <div class="resume-item pb-0">
-          <h4>Graphic Design Skills</h4>
+          <h4>Hard skills</h4>
           <ul>
-            <li>Adobe Creative Suite</li>
-            <li>UX/UI Design</li>
-            <li>Brand Development</li>
-            <li>Illustration</li>
+            <li>Software development and data analysis</li>
+            <li>Reserach skills</li>
+            <li>Aviation</li>
+             <li>Security</li>
+          </ul>
+          <h4>Soft skills</h4>
+          <ul>
+            <li>Team player</li>
+            <li>Leadership</li>
+            <li>Fast Adaptaion Pace</li>
+             <li>Organised</li>
           </ul>
         </div>
 
         <h3 class="resume-title">Education</h3>
         <div class="resume-item">
-          <h4>Master of Fine Arts &amp; Graphic Design</h4>
-          <h5>2015 - 2016</h5>
-          <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+          <h4>Master of Electronic Gov </h4>
+          <h5>2023 - Present</h5>
+          <p><em>Uni Koblenz, Koblenz, DE</em></p>
         </div>
         <div class="resume-item">
-          <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-          <h5>2010 - 2014</h5>
-          <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+          <h4>Bachelor of Computer Sceince</h4>
+          <h5>2013 - 2017</h5>
+          <p><em>University of Ghana, LEG, GH</em></p>
+        </div>
+         <div class="resume-item">
+          <h4>AIR TRAFFIC CONTROL AND OPERATIONS </h4>
+          <h5>2020 - 2022</h5>
+          <p><em>Airforce Aviation School, BC, GH</em></p>
         </div>
       </div>
       
       <div class="col-lg-6">
         <h3 class="resume-title">Professional Experience</h3>
         <div class="resume-item">
-          <h4>Senior Graphic Design Specialist</h4>
-          <h5>2019 - Present</h5>
-          <p><em>Experion, New York, NY</em></p>
+          <h4>IT officer</h4>
+          <h5>2017 - 2017</h5>
+          <p><em>Environmental Protection Agency,
+Accra Ghana, EPA HQ</em></p>
           <ul>
-            <li>Lead design and production communication materials.</li>
-            <li>Supervised design team of 7 members.</li>
-            <li>Managed project budgets ranging from $2,000 - $25,000.</li>
+            <li>A team of 2 under the supervision of the IT
+administrator migrated the agency ’s manual operations
+to automated processes</li>
+            <li>Provided Assistance to Senior Staff at Technical Review
+Committee (TRC) Meetings</li>
+            <li>Monitored performance and maintain systems according
+to requirements</li>
+          </ul>
+        </div>
+        
+
+          <div class="resume-item">
+          <h4>Software engineer</h4>
+          <h5>2017 - 2017</h5>
+          <p><em>MAERSK SHIPPING LIMITED,Tema, GH</em></p>
+          <ul>
+            <li>	Designed and implemented software solutions to optimize logistics and operational processes.</li>
+            <li>Collaborated with cross-functional teams to gather requirements and deliver high-quality software products</li>
           </ul>
         </div>
 
         <div class="resume-item">
-          <h4>Graphic Design Specialist</h4>
-          <h5>2017 - 2018</h5>
-          <p><em>Stepping Stone Advertising, New York, NY</em></p>
+          <h4>Aviation Officer</h4>
+          <h5>2017 - Presesnt</h5>
+          <p><em>Armed forces, Ghana, GH</em></p>
           <ul>
-            <li>Developed numerous marketing programs (logos, brochures, infographics, presentations, advertisements).</li>
-            <li>Managed 5 projects at a time under pressure.</li>
-            <li>Consulted with clients on graphic design strategies.</li>
+            <li>Spearheaded air traffic control operations, ensuring seamless and safe air traffic movements</li>
+            <li>Collaborated with a diverse team to coordinate and execute critical missions and exercises</li>
+            <li>Demonstrated exceptional problem-solving skills in high-pressure situations</li>
           </ul>
         </div>
       </div>
